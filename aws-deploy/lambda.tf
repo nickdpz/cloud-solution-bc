79e77344d58c90628df5d8650e0bc259f276426f
nickdpz <ndpastranzamora@gmail.com>
2021-09-04 14:09:59 -0500

    :sparkles: Lambda example

--- aws-deploy/lambda.tf
+++ aws-deploy/lambda.tf
@@ -3,6 +3,7 @@ resource "aws_lambda_function" "signed_urls_lambda" {
   role          = aws_iam_role.signed_urls_lambda_role.arn
   handler       = "exports.handler"
   runtime       = "nodejs14.x"
+  source_code_hash = filebase64sha256("./files/main.zip")
   tags          = local.common_tags
 
   depends_on = [
@@ -16,6 +17,7 @@ resource "aws_lambda_function" "verify_file_and_notify_lambda" {
   role          = aws_iam_role.verify_file_and_notify_lambda_role.arn
   handler       = "exports.handler"
   runtime       = "nodejs14.x"
+  source_code_hash = filebase64sha256("./files/main.zip")
   tags          = local.common_tags
 
   depends_on = [
