e5ee8132bfb121e82371e7a9e8985c8d1159a71a
nickdpz <ndpastranzamora@gmail.com>
2021-09-06 00:16:29 -0500

    :pencil: Update README with stack

--- aws-deploy/data.tf
+++ aws-deploy/data.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "lambda_assume_role_policy_document" {
   statement {
-    actions = ["sts:AssumeRole"]
     effect  = "Allow"
+    actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
@@ -11,19 +11,20 @@ data "aws_iam_policy_document" "lambda_assume_role_policy_document" {
 
 data "aws_iam_policy_document" "logging_policy_document" {
   statement {
+    effect    = "Allow"
     actions = [
       "logs:CreateLogGroup",
       "logs:CreateLogStream",
       "logs:PutLogEvents"
     ]
-    effect    = "Allow"
     resources = ["arn:aws:logs:*:*:*"]
   }
 }
 
 data "aws_iam_policy_document" "signed_urls_lambda_policy_document" {
   statement {
-    actions = ["s3:Get*"]
+    effect  = "Allow"
+    actions = ["s3:putObjectAcl","s3:putObject"]
     resources = [
       "${aws_s3_bucket.main_bucket.arn}/request",
       aws_s3_bucket.main_bucket.arn
@@ -33,6 +34,7 @@ data "aws_iam_policy_document" "signed_urls_lambda_policy_document" {
 
 data "aws_iam_policy_document" "verify_file_and_notify_lambda_policy_document" {
   statement {
+    effect  = "Allow"
     actions = ["s3:Put*", "s3:Get*"]
     resources = [
       "${aws_s3_bucket.main_bucket.arn}/response",
@@ -42,6 +44,7 @@ data "aws_iam_policy_document" "verify_file_and_notify_lambda_policy_document" {
   }
 
   statement {
+    effect = "Allow"
     actions = [
       "ses:SendEmail",
       "ses:SendRawEmail",
@@ -52,10 +55,23 @@ data "aws_iam_policy_document" "verify_file_and_notify_lambda_policy_document" {
 
 data "aws_iam_policy_document" "ses_sendemail_policy_document" {
   statement {
+    effect = "Allow"
     actions = [
       "ses:SendEmail",
       "ses:SendRawEmail",
     ]
     resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "s3_access_document_policy" {
+  statement {
+    effect    = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.main_bucket.arn}/request"]
   }
 }
--- aws-deploy/s3.tf
+++ aws-deploy/s3.tf
@@ -1,8 +1,15 @@
 resource "aws_s3_bucket" "main_bucket" {
   bucket = "${var.stack_id}-main-bucket"
-  acl    = "private"
+  #acl    = "private"
   versioning {
     enabled = false
+  }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
   }
 
   lifecycle_rule {
@@ -16,20 +23,15 @@ resource "aws_s3_bucket" "main_bucket" {
   tags = merge({ "Name" : "${var.stack_id}-main-bucket" }, local.common_tags)
 }
 
-resource "aws_s3_bucket" "main_bucket_policy" {
+resource "aws_s3_bucket_policy" "main_bucket_policy" {
   bucket = aws_s3_bucket.main_bucket.id
-  policy = <<EOF
-{
-   "Version":"2012-10-17",
-   "Statement":[
-      {
-         "conditions":[
-            ["content-length-range", 1000, 20000]
-         ]
-      }
-   ]
-}
-EOF
+  policy = data.aws_iam_policy_document.s3_access_document_policy.json
+  # policy = jsonencode({
+  #   "expiration": "2015-12-30T12:00:00.000Z",
+  #   "conditions":[
+  #     ["content-length-range", 1048576, 10485760]
+  #   ]
+  # })
 }
 
 resource "aws_s3_bucket_notification" "main_bucket_notification" {
